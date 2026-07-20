a262f942d6c4b987597126d16507fc7b6ffcd9a7
Hussaini <2282933@cognizant.com>
2026-07-20 13:36:21 +0530

    fixed reported field in config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: '[allure-playwright]',
+  reporter: [['allure-playwright']],
   //reporter : [['./tests/Day20/reporter.ts']],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
