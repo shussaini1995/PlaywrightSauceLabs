0d5b5986eb9b00eab8de493b7d898835fc803d3a
Hussaini <2282933@cognizant.com>
2026-07-20 16:12:43 +0530

    test2

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     headless: process.env.CI ? true : false,
-    screenshot: 'only-on-failure',
+    // screenshot: 'only-on-failure',
   },
 
   /* Configure projects for major browsers */
