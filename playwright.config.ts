08b431750bc359d4390f74c837d6c4a8179f8331
Hussaini <2282933@cognizant.com>
2026-07-20 10:03:01 +0530

    changed reported to default reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,8 +24,8 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  // reporter: 'html',
-  reporter : [['./tests/Day20/reporter.ts']],
+  reporter: 'html',
+  //reporter : [['./tests/Day20/reporter.ts']],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
